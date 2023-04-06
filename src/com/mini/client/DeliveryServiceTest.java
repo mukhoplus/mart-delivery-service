b8c35a0d0d8a39ab6375b4e0a04fb651649d9cf4
gitsegun <gitsegun44@gmail.com>
2023-04-06 13:39:23 +0900

    Add product methods

--- src/com/mini/client/DeliveryServiceTest.java
+++ src/com/mini/client/DeliveryServiceTest.java
@@ -1,7 +1,11 @@
 package com.mini.client;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import com.mini.dao.impl.DeliveryServiceDAOImpl;
-import com.mini.dto.Mart;
+import com.mini.dto.Product;
+import com.mini.exception.NotExistException;
 
 import config.ServerInfo;
 
@@ -9,22 +13,57 @@ public class DeliveryServiceTest {
 
 	public static void main(String[] args) {
 		DeliveryServiceDAOImpl ds = DeliveryServiceDAOImpl.getInstance();
-		
+
 		// �׽�Ʈ �ڵ�
+//		try {
+//			ds.saveMart(new Mart("mukho", "mukhodong"));
+//			ds.deleteMart("mukho");
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+
+		
+		
+		// Product
+		//saveProduct / deleteProduct
+//		try {
+//			ds.saveProduct(new Product(777, "CocaCola", 1000));
+//			ds.deleteProduct(777);
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+
+		//updateProduct
+//		try {
+//			ds.updateProduct(new Product(777, "PepsiCola", 1100));
+//		} catch (SQLException e) {
+//			System.out.println(e.getMessage());
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+
 		try {
-			ds.saveMart(new Mart("mukho", "mukhodong"));
-			ds.deleteMart("mukho");
-		} catch(Exception e) {
+			ds.getProductBySerialNumber(777);
+		}  catch (NotExistException | SQLException e) {
 			System.out.println(e.getMessage());
 		}
 		
+
+		try {
+			ArrayList<Product> list = ds.getAllProducts();
+			for (Product c : list)
+				System.out.println(c);
+		} catch (Exception e) {
+
+		}
+
 	}
-	
+
 	static {
 		try {
 			Class.forName(ServerInfo.DRIVER_NAME);
 			System.out.println("Server is running.");
-		} catch(ClassNotFoundException e){
+		} catch (ClassNotFoundException e) {
 			System.out.println("Server is not running.");
 		}
 	}
--- src/com/mini/dao/impl/DeliveryServiceDAOImpl.java
+++ src/com/mini/dao/impl/DeliveryServiceDAOImpl.java
@@ -22,11 +22,14 @@ import config.ServerInfo;
 public class DeliveryServiceDAOImpl implements DeliveryServiceDAO {
 	// Singleton
 	private static DeliveryServiceDAOImpl dao = new DeliveryServiceDAOImpl();
-	private DeliveryServiceDAOImpl() {}
+
+	private DeliveryServiceDAOImpl() {
+	}
+
 	public static DeliveryServiceDAOImpl getInstance() {
 		return dao;
 	}
-	
+
 	@Override
 	public Connection getConnect() throws SQLException {
 		Connection conn = DriverManager.getConnection(ServerInfo.URL, ServerInfo.USER, ServerInfo.PASS);
@@ -36,33 +39,36 @@ public class DeliveryServiceDAOImpl implements DeliveryServiceDAO {
 
 	@Override
 	public void closeAll(PreparedStatement ps, Connection conn) throws SQLException {
-		if(ps != null) ps.close();
-	    if(conn != null) conn.close();
+		if (ps != null)
+			ps.close();
+		if (conn != null)
+			conn.close();
 	}
 
 	@Override
 	public void closeAll(ResultSet rs, PreparedStatement ps, Connection conn) throws SQLException {
-		if(rs != null) rs.close();
-	    closeAll(ps, conn);
+		if (rs != null)
+			rs.close();
+		closeAll(ps, conn);
 	}
 
 	// Customer
 	@Override
 	public void saveCustomer(Customer customer) throws SQLException, DuplicateException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteCustomer(String id) throws SQLException, NotExistException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void updateCustomer(Customer customer) throws SQLException, NotExistException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -86,28 +92,29 @@ public class DeliveryServiceDAOImpl implements DeliveryServiceDAO {
 	// Mart
 	private boolean isExistMart(String name, Connection conn) throws SQLException {
 		String sql = "SELECT name FROM mart WHERE name=?";
-		
-	    PreparedStatement ps = conn.prepareStatement(sql);
-	    ps.setString(1, name);
-	    ResultSet rs = ps.executeQuery();
-	    
+
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setString(1, name);
+		ResultSet rs = ps.executeQuery();
+
 		return rs.next();
 	}
-	
+
 	@Override
 	public void saveMart(Mart mart) throws SQLException, DuplicateException {
 		Connection conn = null;
 		PreparedStatement ps = null;
-		
+
 		try {
 			conn = getConnect();
-			if(isExistMart(mart.getName(), conn)) throw new DuplicateException("A mart with that name already exists.");
-			
-			String query = "INSERT INTO customer VALUES(?, ?)";
+			if (isExistMart(mart.getName(), conn))
+				throw new DuplicateException("A mart with that name already exists.");
+
+			String query = "INSERT INTO mart VALUES(?, ?)";
 			ps = conn.prepareStatement(query);
 			ps.setString(1, mart.getName());
 			ps.setString(2, mart.getAddress());
-			
+
 			System.out.println(ps.executeUpdate() + "-row is saved.");
 		} finally {
 			closeAll(ps, conn);
@@ -118,11 +125,12 @@ public class DeliveryServiceDAOImpl implements DeliveryServiceDAO {
 	public void deleteMart(String name) throws SQLException, NotExistException {
 		Connection conn = null;
 		PreparedStatement ps = null;
-		
+
 		try {
 			conn = getConnect();
-			if(!isExistMart(name, conn)) throw new NotExistException("There is no mart with that name.");
-			
+			if (!isExistMart(name, conn))
+				throw new NotExistException("There is no mart with that name.");
+
 			String query = "DELETE FROM mart WHERE name=?";
 			ps = conn.prepareStatement(query);
 			ps.setString(1, name);
@@ -139,23 +147,24 @@ public class DeliveryServiceDAOImpl implements DeliveryServiceDAO {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		Mart mart = null;
-		
+
 		try {
 			conn = getConnect();
-			if(!isExistMart(name, conn)) throw new NotExistException("There is no mart with that name.");
-			
+			if (!isExistMart(name, conn))
+				throw new NotExistException("There is no mart with that name.");
+
 			String query = "SELECT name, address FROM mart WHERE name=?";
 			ps = conn.prepareStatement(query);
 			ps.setString(1, name);
 
 			rs = ps.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				mart = new Mart(rs.getString("name"), rs.getString("address"));
 			}
 		} finally {
 			closeAll(rs, ps, conn);
 		}
-		
+
 		return mart;
 	}
 
@@ -165,22 +174,22 @@ public class DeliveryServiceDAOImpl implements DeliveryServiceDAO {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		Mart mart = null;
-		
+
 		try {
 			conn = getConnect();
-			
+
 			String query = "SELECT name, address FROM mart WHERE address=?";
 			ps = conn.prepareStatement(query);
 			ps.setString(1, address);
 
 			rs = ps.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				mart = new Mart(rs.getString("name"), rs.getString("address"));
 			}
 		} finally {
 			closeAll(rs, ps, conn);
 		}
-		
+
 		return mart;
 	}
 
@@ -190,76 +199,175 @@ public class DeliveryServiceDAOImpl implements DeliveryServiceDAO {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		ArrayList<Mart> martList = new ArrayList<>();
-		
+
 		try {
 			conn = getConnect();
-			
+
 			String query = "SELECT name, address FROM mart";
 			ps = conn.prepareStatement(query);
 
 			rs = ps.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				martList.add(new Mart(rs.getString("name"), rs.getString("address")));
 			}
 		} finally {
 			closeAll(rs, ps, conn);
 		}
-		
+
 		return martList;
 	}
 
-	// Product
+// Product
+	private boolean isExistProduct(int serialNumber, Connection conn) throws SQLException {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		String sql = "SELECT SERIAL_NUMBER FROM product WHERE SERIAL_NUMBER=?";
+
+		ps = conn.prepareStatement(sql);
+		ps.setInt(1, serialNumber);
+		rs = ps.executeQuery();
+
+		return rs.next();
+	}
+
 	@Override
 	public void saveProduct(Product product) throws SQLException, DuplicateException {
-		// TODO Auto-generated method stub
-		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		try {
+			conn = getConnect();
+			if (!isExistProduct(product.getSerialNumber(), conn)) {
+				String query = "INSERT INTO product(SERIAL_NUMBER, name, price) VALUES(?,?,?)";
+
+				ps = conn.prepareStatement(query);
+				ps.setInt(1, product.getSerialNumber());
+				ps.setString(2, product.getName());
+				ps.setInt(3, product.getPrice());
+
+				System.out.println(ps.executeUpdate() + "-row is saved.");
+
+			} else {
+				throw new DuplicateException("이미 존재하는 상품입니다.");
+			}
+		} finally {
+			closeAll(ps, conn);
+		}
+
 	}
 
 	@Override
 	public void deleteProduct(int serialNumber) throws SQLException, NotExistException {
-		// TODO Auto-generated method stub
-		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		try {
+			conn = getConnect();
+			if (isExistProduct(serialNumber, conn)) {
+				String query = "DELETE product WHERE SERIAL_NUMBER=?";
+
+				ps = conn.prepareStatement(query);
+				ps.setInt(1, serialNumber);
+				System.out.println(ps.executeUpdate() + "-row is deleted.");
+			} else {
+				throw new NotExistException("삭제할 상품이 존재하지 않습니다.");
+			}
+		} finally {
+			closeAll(ps, conn);
+		}
+
 	}
 
 	@Override
 	public void updateProduct(Product product) throws SQLException, NotExistException {
-		// TODO Auto-generated method stub
-		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		try {
+			conn = getConnect();
+			if (isExistProduct(product.getSerialNumber(), conn)) {
+				String query = "UPDATE product SET name=?, price=? WHERE SERIAL_NUMBER=?";
+				ps = conn.prepareStatement(query);
+
+				ps.setString(1, product.getName());
+				ps.setInt(2, product.getPrice());
+				ps.setInt(3, product.getSerialNumber());
+
+				System.out.println(ps.executeUpdate() + " row UPDATE OK");
+
+			} else {
+				throw new NotExistException("수정할 상품이 존재하지 않습니다.");
+			}
+		} finally {
+			closeAll(ps, conn);
+		}
+
 	}
 
 	@Override
 	public Product getProductBySerialNumber(int serialNumber) throws SQLException, NotExistException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Product product = null;
+		try {
+			conn = getConnect();
+			String query = "SELECT SERIAL_NUMBER, name, price FROM product WHERE SERIAL_NUMBER=?";
+			ps = conn.prepareStatement(query);
+			ps.setInt(1, serialNumber);
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				product = new Product(serialNumber, rs.getString("name"), rs.getInt("price"));
+			} else {
+				throw new NotExistException("");
+			}
+		} finally {
+			closeAll(ps, conn);
+		}
+
+		return product;
 	}
 
 	@Override
 	public ArrayList<Product> getAllProducts() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		ArrayList<Product> productList = new ArrayList<>();
+
+		try {
+			conn = getConnect();
+			String query = "SELECT SERIAL_NUMBER, name, price FROM product";
+			ps = conn.prepareStatement(query);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				productList.add(new Product(rs.getInt("SERIAL_NUMBER"), rs.getString("name"), rs.getInt("price")));
+			}
+		} finally {
+			closeAll(rs, ps, conn);
+		}
+
+		return productList;
 	}
-	
+
+//Customer(장바구니 기능)
 	@Override
 	public void saveProductInList() throws SQLException, NotExistException, NotAvailableException {
-		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	// ProductInList
 	@Override
 	public void saveProductInMart() throws SQLException, DuplicateException, NotExistException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void updateProductInMart()
 			throws SQLException, DuplicateException, NotExistException, CannotUpdateException {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
 	@Override
 	public ArrayList<ProductInMart> getAllProductsInMartByName(String name) throws SQLException, NotAvailableException {
 		// TODO Auto-generated method stub
@@ -271,7 +379,7 @@ public class DeliveryServiceDAOImpl implements DeliveryServiceDAO {
 	public void saveDelivery(String delivaryNumber, String martName, String customerName, ArrayList<ProductInMart> list)
 			throws SQLException, NotExistException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
